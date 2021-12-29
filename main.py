0639d49a35697801e1e9b5dd2db50e4deb62333e
Yasindu Weerawarna <64329680+yasinduweerawarna@users.noreply.github.com>
2021-12-30 01:12:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ TOKEN = '5010988549:AAH_1XMqggEuwsHLA_EBC-cgnGqGhdPojyQ'
 
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-  bot.reply_to(message,"Hello World..!")
+	bot.reply_to(message, "Howdy, how are you doing?")
 
-@bot.message_handler(commands=['help'])
-def send_message(message):
-  bot.send_message(message,"Sorry, Still learning..")
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
 
-bot.polling()
+bot.infinity_polling()
