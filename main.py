ad991fb396e84889cff86016363a4d2d994bcd15
Yasindu Weerawarna <64329680+yasinduweerawarna@users.noreply.github.com>
2021-12-30 00:04:24 +0530

    Uploaded

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+import telebot
+
+bot = telebot.TeleBot(5010988549:AAH_1XMqggEuwsHLA_EBC-cgnGqGhdPojyQ)
+
+@bot.message_handler(commands=['Start'])
+def send_welcome(message):
+  bot.reply_to(message, "Hello World..!")
+
+@bot.message_handler(commands=['help'])
+def send_message(message):
+  bot.send_message(message, "Sorry, Still learning..")
+
+bot.polling()
