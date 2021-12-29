89453bcacf79ca197dbb9c7a450217dc261ae153
Yasindu Weerawarna <64329680+yasinduweerawarna@users.noreply.github.com>
2021-12-30 00:16:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ bot = telebot.TeleBot(5010988549:AAH_1XMqggEuwsHLA_EBC-cgnGqGhdPojyQ)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-  bot.reply_to(message, "Hello World..!")
+  bot.reply_to(message,"Hello World..!")
 
 @bot.message_handler(commands=['help'])
 def send_message(message):
-  bot.send_message(message, "Sorry, Still learning..")
+  bot.send_message(message,"Sorry, Still learning..")
 
 bot.polling()
